ebdf878f8ce3a44f763c7bce23d5ca1b61fd2bed
Dr <d.guignu@gmail.com>
2022-01-28 18:00:14 +0100

    Small fixes

--- agents/agent_alphafour/NN.py
+++ agents/agent_alphafour/NN.py
@@ -2,27 +2,37 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from naf.alphafour.agents.agent_alphafour.neural import createNeuralBoard
+from naf.alphafour.agents.common import string_to_board
+from naf.alphafour.agents.helpers import PLAYER1
+
+
 class Convblock(nn.Module):
     def __init__(self):
         super(Convblock, self).__init__()
-        bn = nn.BatchNorm2d(42)
-        self.conv = nn.Conv2d(1, 42, 3, stride=(1, 1), padding=1)
+
+        self.bn = nn.BatchNorm2d(42)
+        self.conv = nn.Conv2d(1, 42, (3, 3), stride=(1, 1), padding=1)
+
 
     def forward(self, value):
+        value = torch.from_numpy(value)
         value = self.bn(value)
         return nn.ReLU(self.conv(value))
+
 
 class ResBlock(nn.Module):
     def __init__(self, in_channels, out_channels, activation='relu'):
         super().__init__()
         self.expansion = 4
         self.in_channels, self.out_channels, self.activation = in_channels, out_channels, activation
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=(1,1), stride=(1,1), padding=1, bias=False)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), stride=(1, 1), padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=(3,3), stride=(1,1), padding=1, bias=False)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=(1,1), stride=(1,1), padding=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(out_channels*self.expansion)
+        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=(1, 1), stride=(1, 1),
+                               padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
 
     def forward(self, value):
         value = self.conv1(value)
@@ -35,30 +45,32 @@ class ResBlock(nn.Module):
         value = self.bn3(value)
         return value
 
+
 class OutBlock(nn.Module):
-    def init(self):
-        super(OutBlock, self).init()
-        self.conv = nn.Conv2d(42, 3, kernel_size=(1,1)) # value head
+    def __init__(self):
+        super(OutBlock, self).__init__()
+        self.conv = nn.Conv2d(42, 3, kernel_size=(1, 1))  # value head
         self.bn = nn.BatchNorm2d(3)
         self.fc1 = nn.Linear(367, 32)
         self.fc2 = nn.Linear(32, 1)
 
-        self.conv1 = nn.Conv2d(42, 32, kernel_size=(1,1)) # policy head
+        self.conv1 = nn.Conv2d(42, 32, kernel_size=(1, 1))  # policy head
         self.bn1 = nn.BatchNorm2d(32)
         self.logsoftmax = nn.LogSoftmax(dim=1)
         self.fc = nn.Linear(6732, 7)
 
-    def forward(self,s):
-        v = F.relu(self.bn(self.conv(s))) # value head
+    def forward(self, s):
+        v = F.relu(self.bn(self.conv(s)))  # value head
         v = v.view(-1, 367)  # batch_size X channel X height X width
         v = F.relu(self.fc1(v))
         v = torch.tanh(self.fc2(v))
 
-        p = F.relu(self.bn1(self.conv1(s))) # policy head
+        p = F.relu(self.bn1(self.conv1(s)))  # policy head
         p = p.view(-1, 6732)
         p = self.fc(p)
         p = self.logsoftmax(p).exp()
         return p, v
+
 
 class AlphaNet(torch.nn.Module):
     '''
@@ -69,7 +81,7 @@ class AlphaNet(torch.nn.Module):
     def __init__(self) -> None:
         super(AlphaNet, self).__init__()
         self.convLayer = Convblock()
-        self.resLayers = [ResBlock()] * 19
+        self.resLayers = [ResBlock(6,7)] * 19
         self.fullLayer = OutBlock()
 
     # Parameters
@@ -82,3 +94,26 @@ class AlphaNet(torch.nn.Module):
         values = self.fullLayer(values)
         return values
 
+
+if __name__ == "__main__":
+    net = AlphaNet()
+    board = string_to_board("""
+    |==============|
+    |    X         |
+    |    O         |
+    |    X X       |
+    |    O X X     |
+    |  O X O O     |
+    |  O O X X     |
+    |==============|
+    |0 1 2 3 4 5 6 |
+    """)
+    neuralBoard = createNeuralBoard(board, PLAYER1)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    input_size = 3
+    num_classes = 7
+    learning_rate = 0.001
+    batch_size = 64
+    num_epochs = 1
+
+    print(net(neuralBoard).shape)
