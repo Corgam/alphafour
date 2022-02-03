dda8c8b1da5440cb9fd34dc0ee7fbf47ec0015fe
Dr <d.guignu@gmail.com>
2022-02-03 19:45:22 +0100

    fixed conv and res blocks

--- agents/agent_alphafour/NN.py
+++ agents/agent_alphafour/NN.py
@@ -1,24 +1,28 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from naf.alphafour.agents.agent_alphafour.neural import createNeuralBoard
 from naf.alphafour.agents.common import string_to_board
-from naf.alphafour.agents.helpers import PLAYER1
 
 
 class Convblock(nn.Module):
     def __init__(self):
         super(Convblock, self).__init__()
 
-        self.bn = nn.BatchNorm2d(42)
-        self.conv = nn.Conv2d(1, 42, (3, 3), stride=(1, 1), padding=1)
+        self.bn = nn.BatchNorm2d(1)
+        self.conv = nn.Conv2d(1, 42, kernel_size=(3, 3), stride=(1, 1), padding=1)
+
+
 
 
     def forward(self, value):
         value = torch.from_numpy(value)
-        value = self.bn(value)
-        return nn.ReLU(self.conv(value))
+        value = value.type(torch.FloatTensor)
+
+        temp = self.conv(value)
+        #temp = self.bn(value)
+        return F.relu(temp)
 
 
 class ResBlock(nn.Module):
@@ -27,22 +31,23 @@ class ResBlock(nn.Module):
         self.expansion = 4
         self.in_channels, self.out_channels, self.activation = in_channels, out_channels, activation
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), stride=(1, 1), padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
+        #self.bn1 = nn.BatchNorm1d(out_channels)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=(1, 1), stride=(1, 1),
+        #self.bn2 = nn.BatchNorm1d(out_channels)
+        self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=(1, 1), stride=(1, 1),
                                padding=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
+        #self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
 
     def forward(self, value):
+
         value = self.conv1(value)
-        value = self.bn1(value)
-        value = nn.ReLU(value)
+        #value = self.bn1(value)
+        value = F.relu(value)
         value = self.conv2(value)
-        value = self.bn2(value)
-        value = nn.ReLU(value)
+        #value = self.bn2(value)
+        value = F.relu(value)
         value = self.conv3(value)
-        value = self.bn3(value)
+        #value = self.bn3(value)
         return value
 
 
@@ -50,22 +55,22 @@ class OutBlock(nn.Module):
     def __init__(self):
         super(OutBlock, self).__init__()
         self.conv = nn.Conv2d(42, 3, kernel_size=(1, 1))  # value head
-        self.bn = nn.BatchNorm2d(3)
+        #self.bn = nn.BatchNorm2d(3)
         self.fc1 = nn.Linear(367, 32)
         self.fc2 = nn.Linear(32, 1)
 
         self.conv1 = nn.Conv2d(42, 32, kernel_size=(1, 1))  # policy head
-        self.bn1 = nn.BatchNorm2d(32)
+        #self.bn1 = nn.BatchNorm2d(32)
         self.logsoftmax = nn.LogSoftmax(dim=1)
         self.fc = nn.Linear(6732, 7)
 
     def forward(self, s):
-        v = F.relu(self.bn(self.conv(s)))  # value head
+        v = F.relu((self.conv(s)))  # value head
         v = v.view(-1, 367)  # batch_size X channel X height X width
         v = F.relu(self.fc1(v))
         v = torch.tanh(self.fc2(v))
 
-        p = F.relu(self.bn1(self.conv1(s)))  # policy head
+        p = F.relu((self.conv1(s)))  # policy head
         p = p.view(-1, 6732)
         p = self.fc(p)
         p = self.logsoftmax(p).exp()
@@ -81,14 +86,14 @@ class AlphaNet(torch.nn.Module):
     def __init__(self) -> None:
         super(AlphaNet, self).__init__()
         self.convLayer = Convblock()
-        self.resLayers = [ResBlock(6,7)] * 19
+        self.resLayers = [ResBlock(42, 42)] * 19
         self.fullLayer = OutBlock()
 
     # Parameters
 
     # Methods
     def forward(self, values):
-        values = self.convLayer(values)
+        values = self.convLayer(np.expand_dims(np.expand_dims(values, 1), 2))
         for layerID in range(19):
             values = self.resLayers[layerID](values)
         values = self.fullLayer(values)
@@ -108,7 +113,7 @@ if __name__ == "__main__":
     |==============|
     |0 1 2 3 4 5 6 |
     """)
-    neuralBoard = createNeuralBoard(board, PLAYER1)
+    neuralBoard = board
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     input_size = 3
     num_classes = 7
